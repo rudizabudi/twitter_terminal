382408c7f5682c9e0fcf7c3baf332ce1795eb72f
rudizabudi <40042979+rudizabudi@users.noreply.github.com>
2025-03-09 15:50:36 +0100

    modified:   .gitignore 	modified:   main.py 	modified:   uv.lock

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from ast import literal_eval
 import asyncio
 from datetime import datetime, timezone
 from dotenv import load_dotenv
-from httpx import ConnectError
+from httpx import ConnectError, ConnectTimeout
 from os import getenv
 from time import sleep
 from twikit import Client, Tweet
@@ -27,25 +27,29 @@ client: Client = Client('en-US')
 post_handler: PostHandler = PostHandler(mirror_discord=MIRROR_DISCORD, webhooks=webhooks)
 
 async def main():
-    await client.login(
-        auth_info_1=USERNAME,
-        auth_info_2=EMAIL,
-        password=PASSWORD,
-        cookies_file='cookies.json')
+    try:
+        await client.login(
+            auth_info_1=USERNAME,
+            auth_info_2=EMAIL,
+            password=PASSWORD,
+            cookies_file='cookies.json')
 
-    i: int = 0
-    while True:
-        while i < len(TWITTER_IDS):
-            try:
-                await ask_tweets(twitter_id=TWITTER_IDS[i], ph=post_handler)
-                sleep(5)
-                i += 1
-            except ConnectError:
-                pass        
-        
-        i = 0
-        post_handler.process_tweets()
-        sleep(60)        
+        while True:
+                while i < len(TWITTER_IDS):
+                    try:
+                        await ask_tweets(twitter_id=TWITTER_IDS[i], ph=post_handler)
+                        sleep(5)
+                        i += 1
+                    except ConnectError:
+                        pass        
+                
+                i = 0
+                post_handler.process_tweets()
+                sleep(60)
+
+    except ConnectTimeout as e:
+        print('Connection timeout, retrying in 1 minute')
+        sleep(60)       
 
 async def ask_tweets(twitter_id: str, ph: PostHandler):
     tweets: list[Tweet] = await client.get_user_tweets(user_id=twitter_id, tweet_type='Tweets', count=20)
