e8d187c83926e90a80d458452bd05d694e4ba8cb
rudizabudi <40042979+rudizabudi@users.noreply.github.com>
2025-03-09 18:09:35 +0100

    modified:   .gitignore 	modified:   main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from httpx import ConnectError
 from os import getenv
 from time import sleep
 from twikit import Client, Tweet
+from twikit.errors import TooManyRequests
 
 from posthandler import PostHandler
 
@@ -27,25 +28,31 @@ client: Client = Client('en-US')
 post_handler: PostHandler = PostHandler(mirror_discord=MIRROR_DISCORD, webhooks=webhooks)
 
 async def main():
-    await client.login(
-        auth_info_1=USERNAME,
-        auth_info_2=EMAIL,
-        password=PASSWORD,
-        cookies_file='cookies.json')
+    try:
+        await client.login(
+            auth_info_1=USERNAME,
+            auth_info_2=EMAIL,
+            password=PASSWORD,
+            cookies_file='cookies.json')
 
-    i: int = 0
-    while True:
-        while i < len(TWITTER_IDS):
-            try:
-                await ask_tweets(twitter_id=TWITTER_IDS[i], ph=post_handler)
-                sleep(5)
-                i += 1
-            except ConnectError:
-                pass        
-        
-        i = 0
-        post_handler.process_tweets()
-        sleep(60)        
+        i: int = 0
+        while True:
+            while i < len(TWITTER_IDS):
+                try:
+                    await ask_tweets(twitter_id=TWITTER_IDS[i], ph=post_handler)
+                    sleep(5)
+                    i += 1
+                except ConnectError:
+                    pass        
+            
+            i = 0
+            post_handler.process_tweets()
+            sleep(60)
+
+    except (TooManyRequests):
+        print('Too many requests. Sleeping for 60 seconds.')
+        sleep(60)
+        await main()        
 
 async def ask_tweets(twitter_id: str, ph: PostHandler):
     tweets: list[Tweet] = await client.get_user_tweets(user_id=twitter_id, tweet_type='Tweets', count=20)
