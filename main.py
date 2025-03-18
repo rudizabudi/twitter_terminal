266142bb3ef66d4e7f58991b6b170eab40d8921d
rudizabudi <40042979+rudizabudi@users.noreply.github.com>
2025-03-18 23:28:11 +0100

    modified:   main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ast import literal_eval
 import asyncio
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 from dotenv import load_dotenv
 from httpx import ConnectError, ConnectTimeout, ReadTimeout
 from os import getenv
@@ -16,12 +16,15 @@ if getenv('DEV_VAR') == 'rudizabudi':
 else:
     load_dotenv('.env')
 
-USERNAME: str = getenv('USERNAME')
-EMAIL: str = getenv('EMAIL')
-PASSWORD: str = getenv('PASSWORD')
-TWITTER_IDS: dict[str, str] = getenv('twitter_ids').split(',')
+INTERVAL: int = 15 #modulo update intervall in mins
+UPDATE_OFFSET: int = 20 #offset to update point in secs
 
-MIRROR_DISCORD: bool = bool(getenv('MIRROR_DISCORD'))
+USERNAME: str = getenv('USERNAME') #twitter
+EMAIL: str = getenv('EMAIL') #twitter
+PASSWORD: str = getenv('PASSWORD') #twitter
+TWITTER_IDS: dict[str, str] = getenv('twitter_ids').split(',') #get from here https://ilo.so/twitter-id/
+
+MIRROR_DISCORD: bool = bool(getenv('MIRROR_DISCORD')) #discord mirror switch
 webhooks: dict[str: list[str]] = literal_eval(getenv('WEBHOOKS'))
 
 client: Client = Client('en-US')
@@ -47,10 +50,19 @@ async def main():
             
             i = 0
             post_handler.process_msgs()
-            sleep(600)
+
+            for min in range(1, INTERVAL + 1):
+                next_update = datetime.now() + timedelta(minutes=min)
+                if next_update.minute % INTERVAL == 0:
+                    break
+
+            next_update = next_update.replace(second=UPDATE_OFFSET, microsecond=0)
+
+            wait_time = next_update - datetime.now()
+            sleep(wait_time.seconds)
 
     except (TooManyRequests, ConnectTimeout, ReadTimeout, AccountSuspended) as e:
-        print(f'Error occured. Sleeping for 60 seconds. {e}')
+        print(f'Error occured. Sleeping for 600 seconds. {e}')
         sleep(600)
         await main()        
 
